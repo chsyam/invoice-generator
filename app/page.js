a100964f5450e9069bf9b8f9ba5a4a5d4898ed73
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-12-25 23:34:35 +0530

    invoice download generator is done

--- app/page.js
+++ app/page.js
@@ -1,101 +1,367 @@
-import Image from "next/image";
+"use client"
+
+import { useRef } from "react";
+import Logo from "./../public/images/logo.png";
+import Signature from "./../public/images/signature.png";
+import styles from "./../styles/invoiceStyles.module.css";
+import { useReactToPrint } from "react-to-print";
+import QrCode from "./../public/images/qrcode.png";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const invoiceRef = useRef(null);
+	const reactToPrintFn = useReactToPrint({ invoiceRef });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const typeOfRecipient = {
+		"Original for Recepient": true,
+		"Duplicate for Transporter": false,
+		"Triplicate for Supplier": false
+	}
+
+	const handleGeneratePDF = async () => {
+		const inputData = invoiceRef.current;
+
+		try {
+			const canvas = await html2canvas(inputData);
+			const imgData = canvas.toDataURL("pdf");
+
+			const pdf = new jsPDF({
+				orientation: 'l',
+				unit: 'px',
+				format: "a4"
+			})
+
+			// const width = pdf.internal.pageSize.getWidth();
+			// const height = (canvas.height * width) / canvas.width;
+
+			// pdf.addImage(imgData, "pdf", 0, 0, width, height);
+			// pdf.save("invoice.pdf");
+			// Adjust dimensions based on your needs
+			const pageWidth = pdf.internal.pageSize.getWidth();
+			const pageHeight = pdf.internal.pageSize.getHeight();
+			const imageWidth = canvas.width;
+			const imageHeight = canvas.height;
+
+			// Calculate the scale to fit the content into the PDF page
+			const scale = Math.min(pageWidth / imageWidth, pageHeight / imageHeight);
+
+			// Center content in PDF
+			const x = (pageWidth - imageWidth * scale) / 2;
+			const y = (pageHeight - imageHeight * scale) / 2;
+
+			pdf.addImage(imgData, 'PNG', x, y, imageWidth * scale, imageHeight * scale);
+			pdf.save('capture.pdf');
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	return (
+		<div className="text-[16px]">
+			<div className="text-center">
+				<button onClick={() => handleGeneratePDF()}>Print</button>
+			</div>
+			<div ref={invoiceRef} style={{ zoom: 1 }} className="mx-[20px] my-[20px] p-[20px]">
+				<div className="border border-black relative flex justify-between items-center gap-4 flex-nowrap bg-blue-200">
+					<div className="text-2xl font-bold h-[40px] flex-1 leading-10 text-center">
+						TAX INVOICE
+					</div>
+					<div className="absolute right-0">
+						{
+							Object.keys(typeOfRecipient).map((type, index) => {
+								return (
+									typeOfRecipient[type] && (
+										<div key={index}>
+											<span className="px-2">{type}</span>
+										</div>
+									)
+								)
+							})
+						}
+					</div>
+				</div>
+				<div className={styles.invoiceMetadata}>
+					<table>
+						<tbody>
+							{/* <tr>
+								<td rowSpan={5}>
+									<img src={Logo.src} alt="logo image" width={200} height={200} />
+								</td>
+							</tr> */}
+							<tr>
+								<td className="text-2xl font-semibold">
+									Phata Phat Delivery
+								</td>
+							</tr>
+							<tr>
+								<td>
+									11-2-284, BESIDE ALHAMDULILLAH HOTEL, NEAR HABEEB NAGAR PS, NAMPALLY, HYDERABAD, Telengana, 500001
+								</td>
+							</tr>
+							<tr>
+								<td className="font-semibold">
+									ph: 8534099966
+								</td>
+							</tr>
+							<tr>
+								<td>
+									GSTIN: <span className="font-bold">36BLAPJ5919F1Z7</span> <br />
+									PAN No: <span className="font-bold">BLAPJ5919F</span>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table>
+						<tbody>
+							<tr>
+								<td>Reverse Charge</td>
+								<td>:</td>
+								<td>No</td>
+							</tr>
+							<tr>
+								<td>Reverse No.</td><td>:</td>
+								<td>385</td>
+							</tr>
+							<tr>
+								<td>Invoice Date</td><td>:</td>
+								<td>30-10-2024</td>
+							</tr>
+							<tr>
+								<td>Date of Supply</td><td>:</td>
+								<td>30-10-2024</td>
+							</tr>
+							<tr>
+								<td>Place of Supply</td><td>:</td>
+								<td>Gachibowli</td>
+							</tr>
+							<tr>
+								<td>State</td><td>:</td>
+								<td>Telangana-36</td>
+							</tr>
+						</tbody>
+					</table>
+					<table>
+						<tbody>
+							<tr>
+								<td>Challan No. </td><td>:</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>Transportation Mode</td>
+								<td>:</td>
+								<td>Road</td>
+							</tr>
+							<tr>
+								<td>Transporter Name</td>
+								<td>:</td>
+								<td>PORTER</td>
+							</tr>
+							<tr>
+								<td>Vehicle No.</td><td>:</td>
+								<td>TS13UD0661</td>
+							</tr>
+							<tr>
+								<td>LR Number</td><td>:</td>
+								<td></td>
+							</tr>
+							<tr>
+								<td>LR Date</td><td>:</td>
+								<td>30-10-2024</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className={styles.shippingDetails}>
+					<div>
+						<div className="font-semibold my-1">Customer Details:</div>
+						<div>Ch Syam Kumar,</div>
+						<div>Ph: 8008331438</div>
+						<div className="font-semibold my-2">Billing Address:</div>
+						<div className="max-w-[400px]">
+							Plot No 41 H NO 2-37/25, Vinayak Nagar, Gachibowli,
+						</div>
+						<div>Hyderabad, Rangareddy</div>
+						<div>Telangana, 500032</div>
+						<div>
+							GSTIN:<span className="font-semibold px-1">36BXBPK0778G1ZG</span>
+						</div>
+					</div>
+					<table>
+						<thead>
+							<tr>
+								<th colSpan={3}>
+									Shipping Address
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Name</td>
+								<td>:</td>
+								<td>M/S PPD</td>
+							</tr>
+							<tr>
+								<td>Address</td>
+								<td>:</td>
+								<td>PLOT NO 41 H NO 2-37/25, VINAYAK NAGAR, GACHIBOWLI, Rangareddy, GACHIBOWLI, Telangana, 500032</td>
+							</tr>
+							<tr>
+								<td>GSTIN</td>
+								<td>:</td>
+								<td>36BXBPK0778G1ZG</td>
+							</tr>
+							<tr>
+								<td>State</td>
+								<td>:</td>
+								<td>Telangana-36</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className={styles.itemsList}>
+					<table>
+						<thead>
+							<tr>
+								<th rowSpan={2}>#</th>
+								<th rowSpan={2}>Name of Product</th>
+								<th rowSpan={2}>HSN/SAC</th>
+								<th rowSpan={2}>QTY</th>
+								<th rowSpan={2}>Unit</th>
+								<th rowSpan={2}>Rate</th>
+								<th rowSpan={2}>Taxable Value</th>
+								<th colSpan={2}>CGST</th>
+								<th colSpan={2}>SGST</th>
+								<th rowSpan={2}>Total</th>
+							</tr>
+							<tr>
+								<th>Rate</th>
+								<th>Amount</th>
+								<th>Rate</th>
+								<th>Amount</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>BASMATI</td>
+								<td>1006</td>
+								<td>3000</td>
+								<td>KGS</td>
+								<td>85.0</td>
+								<td>255,000</td>
+								<td>0.00%</td>
+								<td>0.00</td>
+								<td>0.00%</td>
+								<td>0.00</td>
+								<td>255,000</td>
+							</tr>
+							<tr>
+								<td>
+									<div className="min-h-[100px]"></div>
+								</td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td colSpan={6}>Freight Charges :</td>
+								<td>
+									<div className="text-center">
+										1500.00
+									</div>
+								</td>
+								<td>0.0%</td>
+								<td>0.00</td>
+								<td>0.0%</td>
+								<td>0.00</td>
+								<td>1,500</td>
+							</tr>
+							<tr>
+								<td colSpan={3}>Total Quantity</td>
+								<td colSpan={3}>
+									<div className="text-center">
+										3000
+									</div>
+								</td>
+								<td>₹2,56,500</td>
+								<td colSpan={2}>₹0</td>
+								<td colSpan={2}>₹0</td>
+								<td>₹2,56,500</td>
+							</tr>
+							<tr>
+								<td colSpan={12}>Total invoice amount (in words): INR <span className="font-semibold px-1">{'Ten Thousand Five Hundred Fifty Thousand Only'}.</span> </td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div className={styles.amountDetails}>
+					<table>
+						<thead>
+							<tr>
+								<th colSpan={3}>Bank Details</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Account Number</td>
+								<td>: 025063200000070</td>
+							</tr>
+							<tr>
+								<td>IFSC Code</td>
+								<td>: YESB0000250</td>
+							</tr>
+							<tr>
+								<td>Bank Name</td>
+								<td>: Yes Bank</td>
+							</tr>
+							<tr>
+								<td>Branch</td>
+								<td>: BANJARA HILLS, HYDERABAD</td>
+							</tr>
+							<tr>
+								<td>For UPI Payment</td>
+								<td>: 8534099966</td>
+							</tr>
+						</tbody>
+					</table>
+					<table>
+						<tbody>
+							<tr>
+								<td>Total Amount Before Tax :</td>
+								<td>₹2,56,500</td>
+							</tr>
+							<tr>
+								<td>Add: CGST</td>
+								<td>₹0</td>
+							</tr>
+							<tr>
+								<td>Add: SGST</td>
+								<td>₹0</td>
+							</tr>
+							<tr>
+								<td>Total Amount</td>
+								<td>₹2,56,500</td>
+							</tr>
+							{/* <tr>
+								<td colSpan={2}>
+									<div className="text-center flex flex-col items-center">
+										<img src={Signature.src} alt="signature" width={100} height={100} />
+										Authorised Signatory
+									</div>
+								</td>
+							</tr> */}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 }
