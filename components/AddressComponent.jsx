5e620b82aacb2964d6e55b4598cfe144767c6636
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-12-29 04:26:14 +0530

    completed the invoice

---
+++ components/AddressComponent.jsx
@@ -0,0 +1,32 @@
+export default function AddressComponent() {
+    const address = {
+        'doorNo': '11-2-284',
+        'landmark': 'Beside ALHAMDULILLAH Hotel',
+        'street': 'Near HABEEB Nagar PS',
+        'area': 'NAMPALLY',
+        'city': 'HYDERABAD',
+        'state': 'Telengana',
+        'country': 'IN',
+        'pincode': '500001',
+        'phone': '8534099966',
+        'email': 'syamkumar6845@gmail.com',
+        'gstin': '36BLAPJ5919F1Z7',
+        'pan': 'BLAPJ5919F'
+    }
+    
+    return (
+        <address className="mb-3">
+            <div className="my-2">
+                {`${address?.doorNo}, ${address?.landmark}, ${address?.street}, ${address?.area}, ${address?.state}, ${address?.pincode}, ${address?.city}, ${address?.country}`}
+            </div>
+            <div className="mt-3">
+                Ph: <span className="font-semibold pl-2">8534099966</span><br />
+                Email: <span className="font-semibold pl-2">syamkumar6845@gmail.com</span>
+            </div>
+            <div className="mt-3">
+                GSTIN: <span className="font-semibold pl-2">36BLAPJ5919F1Z7</span><br />
+                PAN No: <span className="font-semibold pl-2">BLAPJ5919F</span>
+            </div>
+        </address>
+    );
+}
---
+++ components/BankComponent.jsx
@@ -0,0 +1,26 @@
+import styles from "./../styles/invoiceStyles.module.css"
+
+export default function BankComponent() {
+    const bankDetails = {
+        'Account Number': '025063200000070',
+        'IFSC Code': 'YESB0000250',
+        'Bank Name': 'Yes Bank',
+        'Branch': 'BANJARA HILLS, HYDERABAD',
+        'For UPI Payment': '8534099966'
+    }
+    console.log(Object.keys(bankDetails))
+    return (
+        <div className="border px-4 py-2 border-t-0 border-black">
+            {
+                Object.keys(bankDetails).map((key, index) => {
+                    return (
+                        <div key={index} className="flex gap-4 my-2">
+                            <div className={styles.key}>{key}</div>
+                            <div className="font-semibold">: {bankDetails[key]}</div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
